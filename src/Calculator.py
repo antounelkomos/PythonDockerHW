6f1c7036bdb2d60660e251b13bfbd330a7b4b9f6
Antoun Elkomos <aae43@njit.edu>
2021-02-24 17:26:12 -0500

    feature: added math operations

--- src/Calculator.py
+++ src/Calculator.py
@@ -2,6 +2,14 @@ def addition(a, b):
     return a + b
 def subtraction(a, b):
     return a - b
+def multiplication(a, b):
+    return a * b
+def division(a, b):
+    return a / b
+def square(a, b):
+    return a ** b
+def root(a, b):
+    return b**(1/float(a))
 
 class Calculator:
     result = 0
@@ -16,3 +24,16 @@ class Calculator:
     def subtract(self, a, b):
         self.result = subtraction(a, b)
         return self.result
+    def multiplication(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+    def division(self, a, b):
+        self.result = division(a, b)
+        return self.result
+    def square(self, a, b):
+        self.result = square(a, b)
+        return self.result
+    def root(self, a, b):
+        self.result = root(a, b)
+        return self.result
+
